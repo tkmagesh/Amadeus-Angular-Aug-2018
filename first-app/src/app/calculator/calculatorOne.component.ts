ca7a3c525bf89663393f331c3ed0ca2251d024a0
Magesh <tkmagesh77@gmail.com>
2018-08-06 16:59:44 +0530

    CalculatorOne - iteration -1

---
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector : 'app-calculator-one',
+	templateUrl : './calculatorOne.component.html'
+})
+export class CalculatorOneComponent{
+	result : number = 0;
+
+	n1 : number = 0;
+	n2 : number = 0;
+
+	setN1(value){
+		this.n1 = value;
+	}
+
+	setN2(value){
+		this.n2 = value;
+	}
+	onAddClick(){
+		this.result = this.n1 + this.n2;
+	}
+
+	onSubtractClick(){
+		this.result = this.n1 - this.n2;
+	}
+
+	onMultiplyClick(){
+		this.result = this.n1 * this.n2;
+	}
+
+	onDivideClick(){
+		this.result = this.n1 / this.n2;
+	}
+}
